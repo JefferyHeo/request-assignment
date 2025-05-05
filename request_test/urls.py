4d8c4e12bdd80d432e48f178eada94da9d1141b6
JefferyHeo <root@LAPTOP-09BCTFUJ.localdomain>
2025-05-06 00:39:28 +0900

    과제6: request 객체 실습 완성

---
+++ request_test/urls.py
@@ -0,0 +1,9 @@
+# request_test/urls.py
+
+from django.urls import path
+from .views import request_info_view, file_upload_view
+
+urlpatterns = [
+    path('request-info/', request_info_view, name='request_info'),
+    path('upload/', file_upload_view, name='upload_file'),
+]
